1b077fb5362810693a2e9da4d792109fb6268cfa
Jan T. Sott <git@idleberg.com>
2023-05-21 22:38:34 +0200

    add tests

---
+++ tests/default.mjs
@@ -0,0 +1,59 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { dirname, resolve } from 'node:path';
+import { promises as fs } from 'node:fs';
+import { Dent } from '../dist/index.mjs';
+
+const __dirname = resolve(dirname(''));
+
+test('Tab indentation', async t => {
+	const dent = new Dent();
+
+	const fixture = await fs.readFile(
+		resolve(__dirname, 'tests/fixtures/indentation.nsi'),
+		'utf8'
+	);
+
+	const expected = await fs.readFile(
+		resolve(__dirname, 'tests/expected/tab-indentation.nsi'),
+		'utf8'
+	);
+
+	assert.is(dent.format(fixture), expected.trim());
+});
+
+test('Space indentation', async t => {
+	const dent = new Dent({
+		useTabs: false
+	});
+
+	const fixture = await fs.readFile(
+		resolve(__dirname, 'tests/fixtures/indentation.nsi'),
+		'utf8'
+	);
+
+	const expected = await fs.readFile(
+		resolve(__dirname, 'tests/expected/space-indentation.nsi'),
+		'utf8'
+	);
+
+	assert.is(dent.format(fixture), expected.trim());
+});
+
+test('Empty lines', async t => {
+	const dent = new Dent();
+
+	const fixture = await fs.readFile(
+		resolve(__dirname, 'tests/fixtures/empty-lines.nsi'),
+		'utf8'
+	);
+
+	const expected = await fs.readFile(
+		resolve(__dirname, 'tests/expected/empty-lines.nsi'),
+		'utf8'
+	);
+
+	assert.is(dent.format(fixture), expected.trim());
+});
+
+test.run();
